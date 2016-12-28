9ca208d63d7590a1a8f2c9de12d697ec6645e6dd
2400bod <bod@gri.kiev.ua>
2016-12-28 18:33:43 +0200

    Single storage for different individual mailboxes

--- homedecor/inbox/init.lua
+++ homedecor/inbox/init.lua
@@ -1,4 +1,8 @@
 local inbox = {}
+
+local filename = minetest.get_worldpath() .. "/mailboxes.dat"
+local mailbox = {}
+
 screwdriver = screwdriver or {}
 
 minetest.register_craft({
@@ -37,75 +41,148 @@ minetest.register_node("inbox:empty", {
 		local owner = placer:get_player_name()
 		meta:set_string("owner", owner)
 		meta:set_string("infotext", owner.."'s Mailbox")
-		local inv = meta:get_inventory()
-		inv:set_size("main", 8*4)
-		inv:set_size("drop", 1)
+		local inv = inbox.get(owner)
+		inbox.save()
 	end,
 	on_rightclick = function(pos, node, clicker, itemstack)
-		local meta = minetest.get_meta(pos)
+		local meta = minetest.get_meta(pos)	
 		local player = clicker:get_player_name()
 		local owner  = meta:get_string("owner")
-		local meta = minetest.get_meta(pos)
+		local inv = inbox.get(owner)
 		if owner == player then
 			minetest.show_formspec(
 				clicker:get_player_name(),
 				"default:chest_locked",
-				inbox.get_inbox_formspec(pos))
+				inbox.get_inbox_formspec(owner))
 		else
 			minetest.show_formspec(
 				clicker:get_player_name(),
 				"default:chest_locked",
-				inbox.get_inbox_insert_formspec(pos))
+				inbox.get_inbox_insert_formspec(owner))
 		end
-		return itemstack
 	end,
 	can_dig = function(pos,player)
 		local meta = minetest.get_meta(pos);
-		local name = player and player:get_player_name()
 		local owner = meta:get_string("owner")
-		local inv = meta:get_inventory()
-		return name == owner and inv:is_empty("main")
-	end,
-	on_metadata_inventory_put = function(pos, listname, index, stack, player)
-		local meta = minetest.get_meta(pos)
-		local inv = meta:get_inventory()
-		if listname == "drop" and inv:room_for_item("main", stack) then
-			inv:remove_item("drop", stack)
-			inv:add_item("main", stack)
-		end
-	end,
-	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-		if listname == "main" then
-			return 0
-		end
-		if listname == "drop" then
-			local meta = minetest.get_meta(pos)
-			local inv = meta:get_inventory()
-			if inv:room_for_item("main", stack) then
-				return -1
-			else
-				return 0
-			end
-		end
+		local inv = inbox.get(owner)
+		return player:get_player_name() == owner and inv:is_empty("main")
 	end,
 })
 
-function inbox.get_inbox_formspec(pos)
-	local spos = pos.x .. "," .. pos.y .. "," ..pos.z
+function inbox.get_inbox_formspec(owner)
 	local formspec =
 		"size[8,9]"..
-		"list[nodemeta:".. spos .. ";main;0,0;8,4;]"..
-		"list[current_player;main;0,5;8,4;]" ..
+		"list[detached:".. owner .."_mailbox;main;0,0;8,4;]"..
+		"list[current_player;main;0,5;8,4;]"
 		"listring[]"
 	return formspec
 end
 
-function inbox.get_inbox_insert_formspec(pos)
-	local spos = pos.x .. "," .. pos.y .. "," ..pos.z
+function inbox.get_inbox_insert_formspec(owner)
 	local formspec =
 		"size[8,9]"..
-		"list[nodemeta:".. spos .. ";drop;3.5,2;1,1;]"..
-		"list[current_player;main;0,5;8,4;]"..
+		"list[detached:".. owner .. "_mailbox;drop;3.5,2;1,1;]"..
+		"list[current_player;main;0,5;8,4;]"
 		"listring[]"
 	return formspec
+end
+
+function inbox.load()
+	local file = io.open(filename, "r")
+
+	if not file then
+		if inbox.save() then
+			file = io.open(filename, "r")
+			if not file then return end
+		else
+			return
+		end
+	end
+
+	local data = file:read("*all")
+	file:close()
+
+	return minetest.deserialize(data)
+end
+
+function inbox.save()
+	local file = io.open(filename, "w")
+	if not file then return end
+
+	local data = minetest.serialize(mailbox)
+
+	file:write(data)
+	file:close()
+	return true
+end
+
+function inbox.update(player_name, inv)
+	mailbox[player_name] = {}
+
+	for index, stack in ipairs( inv:get_list("main") ) do
+		if stack:to_string() ~= "" then
+			mailbox[player_name][index] = stack:to_string()
+		end
+	end
+
+	inbox.save()
+end
+
+function inbox.get(owner)
+	local mailbox_inv = minetest.get_inventory({type="detached", name= owner .. "_mailbox"})
+	if not mailbox or not next(mailbox) then mailbox = inbox.load() end
+
+	if not mailbox_inv then
+		mailbox_inv = minetest.create_detached_inventory(owner .. "_mailbox",{
+			on_put = function(inv, listname, index, stack, player)
+				if listname == "drop" and inv:room_for_item("main", stack) then
+					inv:remove_item("drop", stack)
+					inv:add_item("main", stack)
+					inbox.update(owner, inv)
+					minetest.log("action",
+						player:get_player_name() .. " put '" .. stack:to_string() ..
+						"' to mailbox " .. owner .. " at " .. minetest.pos_to_string(player:getpos()))
+				end
+			end,
+			on_take = function(inv, listname, index, stack, player)
+				inbox.update(owner, inv)
+				minetest.log("action",
+					player:get_player_name() .. " take '" .. stack:to_string() ..
+					"' from mailbox " .. owner .. " at " .. minetest.pos_to_string(player:getpos()))
+			end,
+			on_move = function(inv, from_list, from_index, to_list, to_index, count, player) end,
+			allow_put = function(inv, listname, index, stack, player)
+				if listname == "drop" then
+					return stack:get_count()
+				else
+					return 0
+				end
+			end,
+			allow_take = function(inv, listname, index, stack, player)
+				if listname == "main" then
+					return stack:get_count()
+				else
+					return 0
+				end
+			end,
+			allow_move = function(inv, from_list, from_index, to_list, to_index, count, player)
+				return 0
+			end,
+		})
+
+		mailbox_inv:set_size("main", 8*4)
+		mailbox_inv:set_size("drop", 1)
+
+		if mailbox[owner] then
+			for _, stack in pairs( mailbox[owner] ) do
+				if stack ~= "" then
+					mailbox_inv:add_item("main", ItemStack(stack))
+				end
+			end
+		else
+			inbox.update(owner, mailbox_inv)
+		end
+	end
+
+	return mailbox_inv
 end
